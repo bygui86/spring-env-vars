78bf48b39310216ce68e3a0f438baddcc5a0ca29
Matteo Baiguini <baiguini.matteo@gmail.com>
2018-08-17 19:11:31 +0200

    Very first commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Spring Environment-Variables
+
+## topics
+* spring boot
+* spring mvc
+* properties from environment variables
+* properties from docker
+* properties from kubernetes
+
+## links
---
+++ src/main/java/com/rabbit/samples/springenvvars/SpringEnvVarsApplication.java
@@ -0,0 +1,12 @@
+package com.rabbit.samples.springenvvars;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringEnvVarsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringEnvVarsApplication.class, args);
+	}
+}
